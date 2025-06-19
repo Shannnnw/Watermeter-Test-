d11784c2a8c8939f711d2091768a59b0a416974a
Shannnnw <91250786+Shannnnw@users.noreply.github.com>
2025-06-19 23:07:54 +0800

    chore: clean up legacy code

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,6 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-Vue.use(Vuex)
+import { createStore } from 'vuex'
 
-const store = new Vuex.Store({
+const store = createStore({
   state: {
     snapshot: {},
     patientList: [],
